da8548fb120663283501280c45264e615ab6c4e3
Marcos Buzatta <145435376+mbuzatta@users.noreply.github.com>
2024-09-29 16:23:34 +0200

    feat:Registration form with HTML, CSS & JavaScript.

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordConfirmation = document.getElementById('password-confirmation');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs() {
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const passwordConfirmationValue = passwordConfirmation.value;
+
+    if (usernameValue === '') {
+        setErrorFor(username, 'Username cannot be blank');
+    } else {
+        setSuccessFor(username);
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+    } else if (!checkEmail(emailValue)) {
+        setErrorFor(email, 'Email is not valid');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (passwordValue === '') {
+        setErrorFor(password, 'Password cannot be blank');
+    } else if (passwordValue.length < 7) {
+        setErrorFor(password, 'Password must be at least 7 characters long');
+    } else {
+        setSuccessFor(password);
+    }
+
+    if (passwordConfirmationValue === '') {
+        setErrorFor(passwordConfirmation, 'Password must');
+    } else if (passwordConfirmationValue !== passwordValue) {
+        setErrorFor(passwordConfirmation, 'The passwords do not match.');
+    } else {
+        setSuccessFor(passwordConfirmation);
+    }
+
+    const formControls = form.querySelectorAll(".form-control");
+
+    const formIsValid = [...formControls].every((formControl) => {
+        return formControl.className === "form-control success";
+    });
+
+    if (formIsValid) {
+        console.log("The form is 100% valid!");
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+    formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function checkEmail(email) {
+    return /^(([^<>()$$$$\\.,;:\s@"]+(\.[^<>()$$$$\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
